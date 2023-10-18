e9b62dca003cc682b1753c3170e49e3462ef4f55
phuongctp15 <phuong.ctp15@gmail.com>
2023-10-19 02:17:38 +0700

    Phuong- Done Generate Auth Token

---
+++ src/Middlewares/authenticate.js
@@ -0,0 +1,47 @@
+import { generalVerifyToken } from "../Services/jwtService.js";
+import User from "../Models/User.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+const authenticate = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+      throw new Error("Bạn cần đăng nhập để thực hiện hành động này!");
+    }
+
+    const accessToken = authHeader && authHeader.split(" ")[1];
+
+    let payload = "";
+    try {
+      payload = generalVerifyToken({
+        accessToken,
+        privateKey: process.env.JWT_PRIVATE,
+      });
+    } catch (error) {
+      if (error instanceof jwt.TokenExpiredError) {
+        throw new Error("Token đã hết hạn!");
+      } else {
+        throw new Error("Token không hợp lệ.");
+      }
+    }
+
+    const { _id } = payload.payload;
+    //get User
+    const user = await User.findById(_id);
+    console.log(">>>user:", user);
+    if (!user) {
+      throw new Error("Không tìm thấy người dùng!");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export default authenticate;
