528a082e9636cdf10ec0dd8b76bf5ab0fae70416
phuongctp15 <phuong.ctp15@gmail.com>
2023-10-26 23:53:09 +0700

    update Service

--- src/Models/Service.js
+++ src/Models/Service.js
@@ -1,18 +1,23 @@
 import mongoose from "mongoose";
 import mongoosePaginate from "mongoose-paginate-v2";
 const ServiceSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-        },
-        extract: {
-            type: String,
-        },
-        price: {
-            type: Number
-        }
+  {
+    name: {
+      type: String,
     },
-    { versionKey: false, timestamps: true }
+    extract: {
+      type: String,
+    },
+    price: {
+      type: Number,
+    },
+    status: {
+      type: Number,
+      enum: [0, 1],
+      default: 1,
+    },
+  },
+  { versionKey: false, timestamps: true }
 );
 ServiceSchema.plugin(mongoosePaginate);
 export default mongoose.model("Service", ServiceSchema);
