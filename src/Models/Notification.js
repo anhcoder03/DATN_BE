6a226743e2678e43a4afa0a32abfb5c910664ba7
phuongctp15 <phuong.ctp15@gmail.com>
2023-11-17 04:32:42 +0700

    model Notification

---
+++ src/Models/Notification.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+import mongoosePaginate from "mongoose-paginate-v2";
+const NotificationSchema = new Schema(
+  {
+    _id: {
+      type: String,
+      required: true,
+    },
+    categoryNotification: {
+      type: String,
+      enum: ["booking", "cancel_schedule", "cancel_examination"], // ["Đặt lịch", "Hủy lich", "Hủy khám"],
+    },
+    customerId: {
+      type: String,
+      ref: "Customer",
+    },
+    examinationId: {
+      type: String,
+      ref: "MedicalExaminationSlip",
+    },
+    doctorId: {
+      type: String,
+      ref: "User",
+    },
+    customer: {
+      _id: String,
+      name: String,
+      phone: String,
+    },
+    status: {
+      type: Number,
+      enum: [0, 1], // [0: Chưa đọc, 1: Đã đọc]
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+NotificationSchema.plugin(mongoosePaginate);
+export default mongoose.model("Notification", NotificationSchema);
